e076b817cb23b104c311d2edcf686b62ade807eb
IhorHolinko <mariupolprosecutor@gmail.com>
2023-01-05 20:36:32 +0100

    Something new with functions

--- script.js
+++ script.js
@@ -352,3 +352,37 @@ console.log(personaMovieDB);
 // }
 // console.log(result);
 
+
+// function showFirstMessage() {
+//     console.log("Hello World!");
+// }
+
+// showFirstMessage();
+
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discount);
+    return function() {}
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) {
+            return undefined;
+        }
+    }
+    console.log('done');
+}
+test();
